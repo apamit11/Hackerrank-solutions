5897995630381e8d84dd457c4c0696906569c910
AMIT KUMAR PANDEY <ap697720@gmail.com>
2018-08-26 18:34:33 +0530

    Create Solution.java

---
+++ Practice/java/Introduction/Java Loops I/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+            {
+             System.out.println(n+" x "+i+" = "+(n*i));
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}

85970150da4f65264d03cdf1335a209253cbb678
AMIT KUMAR PANDEY <ap697720@gmail.com>
2018-08-26 18:28:27 +0530

    Create Solution.java

---
+++ Practice/java/Introduction/Welcome to Java/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+   
+   public static void main(String []argv)
+   {
+      System.out.println("Hello World.");//Write your solution here.
+      System.out.println("Hello Java.");
+   }
+
+}
